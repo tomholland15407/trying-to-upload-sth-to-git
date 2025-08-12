e94ec331e72062b3b31dc84af2ba70f3656e3005
tomholland15407 <viethungchu3@gmail.com>
2025-08-12 22:11:16 +0700

    Second commit

---
+++ hello me.py
@@ -0,0 +1,10 @@
+name = input("Em be cua ban ten la gi? ")
+length = len(name)
+if length >= 12:
+    gender = "con gai"
+elif length <= 12:
+    gender = "con trai"
+else:
+    gender = "con trai"
+
+print(f"\n\nCAM ON BAN DA LAM BAI KHAO SAT, EM BE DANG YEU MANG TEN {name} cua ban co gioi tinh la {gender}!\n")
