eddf7e2fe11ebf7a0a80a292ba95ae023c31bb70
tomholland15407 <viethungchu3@gmail.com>
2025-08-17 21:45:43 +0700

    girtty nitty.py

---
+++ girtty nitty.py
@@ -0,0 +1,14 @@
+print('He\'s a handsome guy!')
+abc = 'Hello'
+print(abc.upper())
+print(abc.isupper())
+print(abc[0])
+print(abc.index('ll'))
+print(abc.replace('l', 'm'))
+num = -3.2
+print(round(num % 2))
+print(f'{num} true friends.')
+print(f'{num} + {num}.')
+print(abs(num))
+print(pow(num, 3))
+print(max(num, 19))
